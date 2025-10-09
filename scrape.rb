4169b6b63175dafaa83d4a6e97adb85adb5e636a
Oyjord <oyjord@gmail.com>
2025-10-09 13:55:52 -0700

    Update scrape.rb

--- scrape.rb
+++ scrape.rb
@@ -18,30 +18,30 @@ lines.each do |line|
     divisions << { division: current_division, teams: teams } if current_division
     current_division = line
     teams = []
-  elsif line =~ /^\D+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+-\d+-\d+-\d+\s+\d+$/
+  else
     tokens = line.split(/\s+/)
-    if tokens.size >= 17
-      name = tokens[0..(tokens.size - 16 - 1)].join(" ")
-      stats = tokens.last(16)
-      teams << {
-        team: name,
-        gp: stats[0].to_i,
-        gr: stats[1].to_i,
-        w: stats[2].to_i,
-        l: stats[3].to_i,
-        otl: stats[4].to_i,
-        sol: stats[5].to_i,
-        pts: stats[6].to_i,
-        pct: stats[7].to_f,
-        rw: stats[8].to_i,
-        row: stats[9].to_i,
-        gf: stats[10].to_i,
-        ga: stats[11].to_i,
-        stk: stats[12],
-        p10: stats[13].to_i,
-        pim: stats[14].to_i
-      }
-    end
+    next unless tokens.size >= 18  # team name + 17 stats
+
+    stats = tokens.last(17)
+    name = tokens[0..(tokens.size - 18)].join(" ")
+    teams << {
+      team: name,
+      gp: stats[0].to_i,
+      gr: stats[1].to_i,
+      w: stats[2].to_i,
+      l: stats[3].to_i,
+      otl: stats[4].to_i,
+      sol: stats[5].to_i,
+      pts: stats[6].to_i,
+      pct: stats[7].to_f,
+      rw: stats[8].to_i,
+      row: stats[9].to_i,
+      gf: stats[10].to_i,
+      ga: stats[11].to_i,
+      stk: stats[12],
+      p10: stats[13].to_i,
+      pim: stats[14].to_i
+    }
   end
 end
 
